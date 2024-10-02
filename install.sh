046ce0ecf737e4256e1672f1fe2681598ef48497
batann <122584289+batann@users.noreply.github.com>
2024-10-02 18:42:20 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ if [[ -f /home/batan/.$i ]]; then
 sudo mv /home/batan/.$i /home/batan/.$i.$(date +%j).bak
 echo -e "\033[35m   >>>   \033[31m Moved \033[34m/home/batan/.$i \033[31m to \033[34m/home/batan/.$i.$(date +%j).bak\033[0m"
 fi
-sudo mv /home/batan/dot/$i /home/batan/.$iecho -e "\033[35m   >>>   \033[31m Moved \033[34m/home/batan/.dot/.$i \033[31m to \033[34m/home/batan/.$i\033[0m"
+sudo mv /home/batan/dot/$i /home/batan/.$i
+echo -e "\033[35m   >>>   \033[31m Moved \033[34m/home/batan/.dot/.$i \033[31m to \033[34m/home/batan/.$i\033[0m"
 
 done
 echo "DONE"
